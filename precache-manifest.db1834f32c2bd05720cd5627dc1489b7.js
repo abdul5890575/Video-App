80e444b29b9cea43c5e9eaa7fb9e72b9ff526683
Abdul Rehman Shaukat <abdulrehman.shoukat@gmail.com>
2020-11-16 17:09:18 -0500

    Updates

---
+++ precache-manifest.db1834f32c2bd05720cd5627dc1489b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cc4676a953b4e70b27b2b0aac0cc0b",
+    "url": "/Video-App/index.html"
+  },
+  {
+    "revision": "45239b691e5e4d2c7503",
+    "url": "/Video-App/static/css/main.0134bdc0.chunk.css"
+  },
+  {
+    "revision": "66468dc80edb7ff5beba",
+    "url": "/Video-App/static/js/2.82d24540.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Video-App/static/js/2.82d24540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45239b691e5e4d2c7503",
+    "url": "/Video-App/static/js/main.aa0cdd0f.chunk.js"
+  },
+  {
+    "revision": "cace3b142722df1cb667",
+    "url": "/Video-App/static/js/runtime-main.8cce80c9.js"
+  }
+]);
